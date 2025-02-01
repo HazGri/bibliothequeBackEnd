25d7cb85fcdc58ab0e25d7c80eb995d91263db14
HazGri <hazemgherissi@gmail.com>
2025-02-01 15:45:28 +0100

    ajout auteur

--- controllers/auteur.controller.js
+++ controllers/auteur.controller.js
@@ -1,27 +1,48 @@
 const mongoose = require("mongoose");
-const fs = require('fs');
-const auteurModel= require("../models/auteurs.model");
+const fs = require("fs");
+const auteurModel = require("../models/auteurs.model");
 
 exports.auteur_affichage = (req, res) => {
-    auteurModel.findById(req.params.id)
-    .exec()
-    .then(auteur => {
-        console.log(auteur);
-        res.render("auteurs/auteur.html.twig", {auteur : auteur});
-    })
-    .catch(error => {
-        console.log(error);
-    })
-}
-
-exports.auteurs_affichage= (req, res) => {
-    auteurModel.find()
+  auteurModel
+    .findById(req.params.id)
     .populate("livres")
     .exec()
-    .then(auteurs => {
-        res.render("auteurs/listeAuteur.html.twig" , {auteurs : auteurs})
+    .then((auteur) => {
+      console.log(auteur);
+      res.render("auteurs/auteur.html.twig", { auteur: auteur });
     })
-    .catch(error => {
-        console.log(error)
+    .catch((error) => {
+      console.log(error);
     });
-}
+};
+
+exports.auteurs_affichage = (req, res) => {
+  auteurModel
+    .find()
+    .populate("livres")
+    .exec()
+    .then((auteurs) => {
+      res.render("auteurs/listeAuteur.html.twig", { auteurs: auteurs });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+exports.auteur_ajout = (req, res) => {
+  const auteur = new auteurModel({
+    _id: new mongoose.Types.ObjectId(),
+    nom: req.body.nom,
+    prenom: req.body.prenom,
+    age: req.body.age,
+    sexe: (req.body.sexe) ? true : false,
+  });
+  auteur
+    .save()
+    .then((resultat) => {
+      res.redirect("/auteurs");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
--- routeurs/auteur.routeur.js
+++ routeurs/auteur.routeur.js
@@ -6,5 +6,6 @@ const auteurController = require("../controllers/auteur.controller")
 
 routeur.get("/:id", auteurController.auteur_affichage)
 routeur.get("/", auteurController.auteurs_affichage)
+routeur.post("/",auteurController.auteur_ajout);
 
 module.exports = routeur;
