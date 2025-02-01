f83f6de32aa74c5baa1dd6bed2fcc06a5ce75d67
HazGri <hazemgherissi@gmail.com>
2025-02-01 18:22:29 +0100

    fin du CRUD auteur

--- controllers/auteur.controller.js
+++ controllers/auteur.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const fs = require("fs");
 const auteurModel = require("../models/auteurs.model");
+const livreModel = require("../models/livres.model");
 
 exports.auteur_affichage = (req, res) => {
   auteurModel
@@ -22,7 +23,7 @@ exports.auteurs_affichage = (req, res) => {
     .populate("livres")
     .exec()
     .then((auteurs) => {
-      res.render("auteurs/listeAuteur.html.twig", { auteurs: auteurs });
+      res.render("auteurs/listeAuteur.html.twig", { auteurs: auteurs, isModification:false });
     })
     .catch((error) => {
       console.log(error);
@@ -46,3 +47,51 @@ exports.auteur_ajout = (req, res) => {
       console.log(error);
     });
 };
+
+exports.auteur_suppression = (req, res) => {
+    auteurModel.find()
+    .where("nom").equals("anonyme")
+    .exec()
+    .then(auteur => {
+        livreModel.updateMany({"auteur":req.params.id},{"$set":{
+            "auteur": auteur[0]._id
+        }}, {"multi": true})
+        .exec()
+        .then(
+            auteurModel.deleteOne({_id:req.params.id})
+            .where("nom").ne("anonyme")
+            .exec()
+            .then(res.redirect("/auteurs"))
+            .catch(error => {
+                console.log(error);
+            })
+        )
+    })
+}
+
+exports.auteur_modif = (req, res) => {
+    auteurModel.findById(req.params.id)
+    .populate("livres")
+    .exec()
+    .then((auteur) => {
+      res.render("auteurs/auteur.html.twig", { auteur: auteur, isModification:true});
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+exports.auteur_modif_validation = (req, res) => {
+    const auteurUpdate = {
+        nom : req.body.nom,
+        prenom : req.body.prenom,
+        age : req.body.age,
+        sexe : (req.body.sexe) ? true : false,
+    };
+    auteurModel.updateOne({_id:req.body.identifiant}, auteurUpdate)
+    .exec()
+    .then(resultat => {
+        res.redirect("/auteurs");
+    })
+    .catch();
+}
--- routeurs/auteur.routeur.js
+++ routeurs/auteur.routeur.js
@@ -4,8 +4,11 @@ var twig = require("twig");
 
 const auteurController = require("../controllers/auteur.controller")
 
-routeur.get("/:id", auteurController.auteur_affichage)
-routeur.get("/", auteurController.auteurs_affichage)
+routeur.get("/:id", auteurController.auteur_affichage);
+routeur.get("/", auteurController.auteurs_affichage);
 routeur.post("/",auteurController.auteur_ajout);
+routeur.post("/delete/:id", auteurController.auteur_suppression);
+routeur.get("/modification/:id", auteurController.auteur_modif);
+routeur.post("/modificationServer",auteurController.auteur_modif_validation)
 
 module.exports = routeur;
